1a7eb4bbcb304845194d1060e2b276e9ddbd3592
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 13:15:24 -0500

    feat: log registered routes in the application bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // Define un prefijo global para la API
   await app.listen(process.env.PORT || 3000);
+  console.log(app.getHttpServer()._events.request._router.stack.map(r => r.route?.path));
+
 }
 bootstrap();
