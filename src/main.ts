cf9ab7be395409d33fe56baddb1cdc834b83cb0e
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 01:15:21 -0500

    update main.ts to listen on the port assigned by Vercel

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000); // Vercel usará el puerto asignado automáticamente
+  await app.listen(process.env.PORT || 3000); // Asegúrate de escuchar en el puerto que Vercel asigna
 }
 bootstrap();
