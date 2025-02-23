2d651778997a4e1010961efb439d050cfa159838
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 12:29:12 -0500

    refactor: move database controller and service to src/database directory

--- src/database/database.controller.ts
+++ src/database/database.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { DatabaseService } from './database.service';
+import { DatabaseService } from 'src/database/database.service';
 
 @Controller('database')
 export class DatabaseController {
