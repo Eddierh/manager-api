f27e014e8099a86a3b33d946060d4e9e10d9bc40
Thenew2 <hernandezeddier@gmail.com>
2025-02-24 00:18:50 -0500

    fix: update output paths in configuration and improve server initialization for Vercel and local environments

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import  createServer from '@vendia/serverless-express'; // ✅ Importación corregida
+import createServer from '@vendia/serverless-express';
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await app.init();
 
-  const expressApp = app.getHttpAdapter().getInstance();
-  return createServer(expressApp); // ✅ Usar createServer directamente
+  if (process.env.VERCEL) {
+    // ✅ Configuración para Vercel (Serverless)
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    return createServer(expressApp);
+  } else {
+    // ✅ Configuración para entorno local
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    console.log(`Servidor ejecutándose en http://localhost:${port}`);
+  }
 }
+
+bootstrap();
