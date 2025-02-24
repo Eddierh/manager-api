7b0fa77703871dee60cd6dc1f05ec82208b8cc76
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 22:07:42 -0500

    feat: update serverless handler export and enhance Vercel configuration

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import serverlessExpress from '@vendia/serverless-express';
 
+// Exportamos directamente el servidor inicializado
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); // Mantener el prefijo global
+  app.setGlobalPrefix('api'); // Prefijo global
   await app.init();
 
-  // Convertir la aplicación de NestJS en una función serverless
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
 
-// Exportar como función por defecto para que Vercel la reconozca
-export default bootstrap().then((handler) => handler);
+// Exportamos el servidor inicializado como handler
+const server = bootstrap();
+export default server;
