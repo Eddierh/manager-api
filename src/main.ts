b0990823e2682f12b70cc7711e815360c75a62d3
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 20:42:02 -0500

    feat: update Vercel configuration to include builds section and maintain global prefix in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api'); // Mantener el prefijo global
   await app.listen(port);
 }
 bootstrap();
