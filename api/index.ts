3b29d3e2745698f3c8fa2f599bf3914722f553c9
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 22:57:10 -0500

    fix: update bootstrap import path in API index for correct server initialization

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,11 @@
 const path = require('path');
-const { bootstrap } = require(path.resolve(__dirname, '../dist/main.js'));
 
+// ✅ Usar 'let' fuera de cualquier función evita múltiples declaraciones en compilaciones
 let server;
 
 module.exports = async function handler(event, context, callback) {
   if (!server) {
+    const { bootstrap } = require(path.resolve(__dirname, '../dist/src/main.js'));
     server = await bootstrap();
   }
   return server(event, context, callback);
