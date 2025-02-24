a9e0cf712427f66b41b9df29dcf0fc1e241fb601
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 20:09:32 -0500

    feat: update main.ts to use environment port and modify Vercel configuration for deployment

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); // Define un prefijo global para la API
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000; // Usar el puerto proporcionado por Vercel
+  app.setGlobalPrefix('api');
+  await app.listen(port);
 }
 bootstrap();
