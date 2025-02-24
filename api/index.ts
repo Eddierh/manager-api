b1b5f20ac226bf2553c5bd5e972dad7a262c0ace
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 22:51:28 -0500

    fix: update API handler to use CommonJS module syntax and improve bootstrap import

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,10 @@
+const { bootstrap } = require('../dist/main.js');
+
 let server: any;
 
-export default async function handler(event: any, context: any, callback: any) {
+module.exports = async function handler(event: any, context: any, callback: any) {
   if (!server) {
-    const isProd = process.env.NODE_ENV === 'production';
-    const path = isProd ? './main.js' : '../dist/main.js'; // Cambiar la ruta según el entorno
-
-    const { bootstrap } = await import(path);
     server = await bootstrap();
   }
   return server(event, context, callback);
-}
+};
