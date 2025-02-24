0d73804f8ff2d0e6911fd1727a10d9cda38dd75d
Thenew2 <hernandezeddier@gmail.com>
2025-02-23 22:43:07 -0500

    fix: dynamically import bootstrap based on environment for improved server initialization

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,11 @@
-import { bootstrap } from '../dist/main';
-
 let server: any;
 
 export default async function handler(event: any, context: any, callback: any) {
   if (!server) {
+    const isProd = process.env.NODE_ENV === 'production';
+    const path = isProd ? './main.js' : '../dist/main.js'; // Cambiar la ruta según el entorno
+
+    const { bootstrap } = await import(path);
     server = await bootstrap();
   }
   return server(event, context, callback);
